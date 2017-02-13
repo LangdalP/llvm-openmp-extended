e8888026717c55b765da84ca7e6d1a5971ed786e
Peder Voldnes Langdal <peder.langdal@gmail.com>
2017-02-13 18:53:46 +0100

    T. creation should now be correct for regular tasks

--- runtime/src/kmp_tasking.cpp
+++ runtime/src/kmp_tasking.cpp
@@ -1444,13 +1444,41 @@ __kmp_omp_task( kmp_int32 gtid, kmp_task_t * new_task, bool serialize_immediate 
     }
 #endif
 
+#if OMP_45_ENABLED
+    if ( new_taskdata->td_flags.proxy == TASK_PROXY ) {
+        kmp_taskdata_t * current_task = __kmp_threads[ gtid ] -> th.th_current_task;
+        if ( serialize_immediate )
+            new_taskdata -> td_flags.task_serial = 1;
+        __kmp_invoke_task( gtid, new_task, current_task );
+    }
+#endif
+
+    // Attempt to push
+    kmp_int32 push_ret = __kmp_push_task( gtid, new_task );
+
+    // PVL: ompt_callback_task_create moved here so that  measured task creation duration is more accurate
+#if OMPT_SUPPORT
+    kmp_taskdata_t *parent;
+    if (ompt_enabled) {
+        parent = new_taskdata->td_parent;
+        parent->ompt_task_info.frame.reenter_runtime_frame =
+            OMPT_GET_FRAME_ADDRESS(1);
+        if (ompt_callbacks.ompt_callback(ompt_callback_task_create)) {
+            ompt_task_data_t task_data = ompt_task_id_none;
+            ompt_callbacks.ompt_callback(ompt_callback_task_create)(
+                parent ? &(parent->ompt_task_info.task_data) : &task_data,
+                parent ? &(parent->ompt_task_info.frame) : NULL,
+                &(new_taskdata->ompt_task_info.task_data),
+                ompt_task_explicit,
+                0,
+                new_taskdata->ompt_task_info.function);
+        }
+    }
+#endif
+
     /* Should we execute the new task or queue it?   For now, let's just always try to
        queue it.  If the queue fills up, then we'll execute it.  */
-#if OMP_45_ENABLED
-    if ( new_taskdata->td_flags.proxy == TASK_PROXY || __kmp_push_task( gtid, new_task ) == TASK_NOT_PUSHED ) // if cannot defer
-#else
-    if ( __kmp_push_task( gtid, new_task ) == TASK_NOT_PUSHED ) // if cannot defer
-#endif
+    if ( push_ret == TASK_NOT_PUSHED ) // if cannot defer
     {                                                           // Execute this task immediately
         kmp_taskdata_t * current_task = __kmp_threads[ gtid ] -> th.th_current_task;
         if ( serialize_immediate )
@@ -1491,31 +1519,16 @@ __kmpc_omp_task( ident_t *loc_ref, kmp_int32 gtid, kmp_task_t * new_task)
     KA_TRACE(10, ("__kmpc_omp_task(enter): T#%d loc=%p task=%p\n",
                   gtid, loc_ref, new_taskdata ) );
 
-#if OMPT_SUPPORT
-    kmp_taskdata_t *parent;
-    if (ompt_enabled) {
-        parent = new_taskdata->td_parent;
-        parent->ompt_task_info.frame.reenter_runtime_frame =
-            OMPT_GET_FRAME_ADDRESS(1);
-        if (ompt_callbacks.ompt_callback(ompt_callback_task_create)) {
-            ompt_task_data_t task_data = ompt_task_id_none;
-            ompt_callbacks.ompt_callback(ompt_callback_task_create)(
-                parent ? &(parent->ompt_task_info.task_data) : &task_data,
-                parent ? &(parent->ompt_task_info.frame) : NULL,
-                &(new_taskdata->ompt_task_info.task_data),
-                ompt_task_explicit,
-                0,
-                new_taskdata->ompt_task_info.function);
-        }
-    }
-#endif
+    // PVL: OMPT callback for ompt_callback_task_create used to be here
 
     res =  __kmp_omp_task(gtid,new_task,true);
 
     KA_TRACE(10, ("__kmpc_omp_task(exit): T#%d returning TASK_CURRENT_NOT_QUEUED: loc=%p task=%p\n",
                   gtid, loc_ref, new_taskdata ) );
 #if OMPT_SUPPORT
+    kmp_taskdata_t *parent;
     if (ompt_enabled) {
+        parent = new_taskdata->td_parent;
         parent->ompt_task_info.frame.reenter_runtime_frame =
             NULL;
     }
